c608d1f37b0106e05268b86abdad287930a2d5e7
Anders <andersroseen@gmail.com>
2021-02-22 15:41:47 +0100

    Create new dummy endpoint (/user)

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Artportable.API.Entities.Model;
+
+namespace Artportable.API.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    // [Authorize]
+    public class UserController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetUser()
+        {
+          var user = new User() { Name = "Kalle Banan" };
+
+          return Ok(user);
+        }
+    }
+}
---
+++ Entities/Model/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Artportable.API.Entities.Model
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
