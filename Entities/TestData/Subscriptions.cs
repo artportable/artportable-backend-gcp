fe145ad4c838b66e32c4d89eaf5069fc2bbc1b7d
Anders <andersroseen@gmail.com>
2021-03-26 19:59:52 +0100

    Add a few users

--- Entities/TestData/Subscriptions.cs
+++ Entities/TestData/Subscriptions.cs
@@ -13,44 +13,114 @@ namespace Artportable.API.Testing
           new Subscription
           {
             Id = 1,
-            ProductId = 1,
-            ExpirationDate = null,
-            CustomerId = null
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(21),
+            CustomerId = "cus_J6O5PTOI0UfdjD"
           },
           new Subscription
           {
             Id = 2,
-            ProductId = 2,
-            ExpirationDate = DateTime.Now.AddDays(21),
-            CustomerId = "testcujlkd13543a"
-          },
-          new Subscription
-          {
-            Id = 3,
-            ProductId = 3,
-            ExpirationDate = DateTime.Now.AddDays(25),
-            CustomerId = "testcujlkd32132dsa"
-          },
-          new Subscription
-          {
-            Id = 4,
             ProductId = 1,
             ExpirationDate = null,
             CustomerId = null
           },
           new Subscription
           {
+            Id = 3,
+            ProductId = 1,
+            ExpirationDate = null,
+            CustomerId = null
+          },
+          new Subscription
+          {
+            Id = 4,
+            ProductId = 3,
+            ExpirationDate = DateTime.Now.AddDays(25),
+            CustomerId = "cus_J6O5PTOewXfdjD"
+          },
+          new Subscription
+          {
             Id = 5,
-            ProductId = 2,
-            ExpirationDate = DateTime.Now.AddDays(6),
-            CustomerId = "testcsasaSAdsa"
+            ProductId = 1,
+            ExpirationDate = null,
+            CustomerId = null
           },
           new Subscription
           {
             Id = 6,
             ProductId = 2,
             ExpirationDate = DateTime.Now.AddDays(46),
-            CustomerId = "testcdsdsalkdsa"
+            CustomerId = "cus_J6O521fI1XfdjD"
+          },
+          new Subscription
+          {
+            Id = 7,
+            ProductId = 1,
+            ExpirationDate = null,
+            CustomerId = null
+          },
+          new Subscription
+          {
+            Id = 8,
+            ProductId = 3,
+            ExpirationDate = DateTime.Now.AddDays(132),
+            CustomerId = "cus_J6O5PTOI1XfdjW"
+          },
+          new Subscription
+          {
+            Id = 9,
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(15),
+            CustomerId = "cus_J6O5PTOI6XfdjD"
+          },
+          new Subscription
+          {
+            Id = 10,
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(30),
+            CustomerId = "cus_J6O5PTOI2XfdjD"
+          },
+          new Subscription
+          {
+            Id = 11,
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(29),
+            CustomerId = "cus_J6O5JKOI1XfdjD"
+          },
+          new Subscription
+          {
+            Id = 12,
+            ProductId = 1,
+            ExpirationDate = null,
+            CustomerId = null
+          },
+          new Subscription
+          {
+            Id = 13,
+            ProductId = 1,
+            ExpirationDate = null,
+            CustomerId = null
+          },
+          new Subscription
+          {
+            Id = 14,
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(86),
+            CustomerId = "cus_J6O5PTOIdXfdjD"
+          },
+          new Subscription
+          {
+            Id = 15,
+            ProductId = 3,
+            ExpirationDate = DateTime.Now.AddDays(12),
+            CustomerId = "cus_J6OsPTOI1XfdjD"
+          },
+          new Subscription
+          {
+            Id = 16,
+            ProductId = 2,
+            ExpirationDate = DateTime.Now.AddDays(26),
+            CustomerId = "cus_J6O5PTdI1XfdjD"
           }
         };
     }
--- Entities/TestData/Users.cs
+++ Entities/TestData/Users.cs
@@ -15,10 +15,10 @@ namespace Artportable.API.Testing
             Id = 1,
             PublicId = new Guid("b2ca9be2-f852-4d65-9498-c43366996352"),
             SubscriptionId = 1,
-            FileId = 15,
-            Username = "lordtep",
-            Email = "lord@tep.com",
-            Created = DateTime.Now,
+            FileId = 24,
+            Username = "jimpa",
+            Email = "jimmy@boulder.se",
+            Created = new DateTime(2021, 02, 22, 09, 00, 12),
             Language = "sv"
           },
           new User
@@ -26,10 +26,10 @@ namespace Artportable.API.Testing
             Id = 2,
             PublicId = new Guid("39d044e3-6936-4c18-85d0-9d0b1ed5164e"),
             SubscriptionId = 2,
-            FileId = 16,
+            FileId = 63,
             Username = "andersand",
-            Email = "anders@anders.and",
-            Created = DateTime.Now,
+            Email = "anders@boulder.se",
+            Created = new DateTime(2021, 02, 22, 13, 07, 48),
             Language = "sv"
           },
           new User
@@ -37,10 +37,10 @@ namespace Artportable.API.Testing
             Id = 3,
             PublicId = new Guid("6b4282b6-3014-40cd-9de3-a3f29f10bb31"),
             SubscriptionId = 3,
-            FileId = 17,
+            FileId = 55,
             Username = "ludde",
-            Email = "lud@wig.se",
-            Created = DateTime.Now,
+            Email = "ludwig@boulder.se",
+            Created = new DateTime(2020, 04, 17, 17, 15, 15),
             Language = "sv"
           },
           new User
@@ -48,10 +48,10 @@ namespace Artportable.API.Testing
             Id = 4,
             PublicId = new Guid("857ce515-b7dd-4eae-991b-20468cf33ec3"),
             SubscriptionId = 4,
-            FileId = 18,
+            FileId = 123,
             Username = "sillynilly",
-            Email = "niclas@hej.hopp",
-            Created = DateTime.Now,
+            Email = "niclas@boulder.se",
+            Created = new DateTime(2020, 05, 28, 20, 32, 10),
             Language = "sv"
           },
           new User
@@ -59,10 +59,10 @@ namespace Artportable.API.Testing
             Id = 5,
             PublicId = new Guid("820d9ee1-573e-4c4b-aeec-b077a793e26f"),
             SubscriptionId = 5,
-            FileId = 19,
-            Username = "linkanboy",
-            Email = "li@n.us",
-            Created = DateTime.Now,
+            FileId = 139,
+            Username = "linus",
+            Email = "linus@boulder.se",
+            Created = new DateTime(2020, 09, 12, 23, 55, 48),
             Language = "sv"
           },
           new User
@@ -70,10 +70,120 @@ namespace Artportable.API.Testing
             Id = 6,
             PublicId = new Guid("939dbb39-9250-43c7-b1d5-fe879ccf4167"),
             SubscriptionId = 6,
-            FileId = 20,
-            Username = "kallebanan",
-            Email = "kalle@banan.se",
-            Created = DateTime.Now,
+            FileId = 143,
+            Username = "svennebanan",
+            Email = "svenne@engammalteliamejl.se",
+            Created = new DateTime(2021, 03, 26, 11, 00, 51),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 7,
+            PublicId = new Guid("0dfa4504-7369-4669-ba26-aa390161d573"),
+            SubscriptionId = 7,
+            FileId = 145,
+            Username = "lenagranris",
+            Email = "lena@granris.se",
+            Created = new DateTime(2021, 02, 21, 01, 00, 49),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 8,
+            PublicId = new Guid("e35a7bc6-3d87-4fd4-a954-d6aa7cd4f33e"),
+            SubscriptionId = 8,
+            FileId = 29,
+            Username = "picasso",
+            Email = "pablo.r.picasso@louvre.fr",
+            Created = new DateTime(2021, 01, 18, 17, 05, 42),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 9,
+            PublicId = new Guid("6c8f43b4-bd72-40f5-a0cd-0b2c386f02d0"),
+            SubscriptionId = 9,
+            FileId = 98,
+            Username = "vangogh",
+            Email = "vincent@vangogh.nl",
+            Created = new DateTime(2021, 03, 20, 10, 01, 22),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 10,
+            PublicId = new Guid("7594fde4-2e74-4d4f-a60a-548be2d09460"),
+            SubscriptionId = 10,
+            FileId = 33,
+            Username = "konstnarssjal",
+            Email = "alva@konstnarerna.no",
+            Created = new DateTime(2021, 01, 30, 07, 21, 09),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 11,
+            PublicId = new Guid("60f8ba1d-7468-45f3-876e-add0809fc760"),
+            SubscriptionId = 11,
+            FileId = 46,
+            Username = "glenna",
+            Email = "glennaeilidh@gemajl.com",
+            Created = new DateTime(2021, 01, 07, 18, 22, 03),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 12,
+            PublicId = new Guid("eef3bbe3-5b1d-4c03-88a2-1b1c42acaf5d"),
+            SubscriptionId = 12,
+            FileId = 73,
+            Username = "maggan",
+            Email = "maggan827@hawtmajl.com",
+            Created = new DateTime(2021, 01, 20, 18, 01, 48),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 13,
+            PublicId = new Guid("fb54dbb3-c885-4fbb-9f78-77cdfe89855e"),
+            SubscriptionId = 13,
+            FileId = 64,
+            Username = "vilhelminah",
+            Email = "vhenrika@växjsöskola.se",
+            Created = new DateTime(2021, 02, 27, 23, 05, 28),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 14,
+            PublicId = new Guid("80a2bdc3-ba99-4f1c-a6bd-8c31ea1f02c9"),
+            SubscriptionId = 14,
+            FileId = 88,
+            Username = "kaja",
+            Email = "kaja@juditskonst.se",
+            Created = new DateTime(2021, 03, 05, 14, 37, 21),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 15,
+            PublicId = new Guid("c29ebbf0-ebdd-4f96-952c-6ab96e888f1e"),
+            SubscriptionId = 15,
+            FileId = 120,
+            Username = "elenafromstpete",
+            Email = "elena@paint.ru",
+            Created = new DateTime(2021, 01, 13, 15, 35, 19),
+            Language = "sv"
+          },
+          new User
+          {
+            Id = 16,
+            PublicId = new Guid("182fa258-53c2-4000-8f6e-581c69caf561"),
+            SubscriptionId = 16,
+            FileId = 139,
+            Username = "cartoonguy",
+            Email = "hello@cartoonguy.com",
+            Created = new DateTime(2021, 02, 18, 16, 45, 55),
             Language = "sv"
           }
         };
