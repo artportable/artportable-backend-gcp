475c7cfb2bb8a84189351e89dba348d6ab89dbfb
Anders <andersroseen@gmail.com>
2021-04-13 15:39:51 +0200

    Add Artwork Tags connections to test data

---
+++ Entities/TestData/ArtworksTags.cs
@@ -0,0 +1,67 @@
+namespace Artportable.API.Testing
+{
+  public partial class Data
+  {
+    public object[] ArtworksTags
+    {
+      get =>
+        new []
+        {
+          new { ArtworksId = 1, TagsId = 9 },
+          new { ArtworksId = 1, TagsId = 10 },
+          new { ArtworksId = 1, TagsId = 16 },
+          new { ArtworksId = 1, TagsId = 17 },
+
+          new { ArtworksId = 2, TagsId = 1 },
+          new { ArtworksId = 2, TagsId = 11 },
+
+          new { ArtworksId = 3, TagsId = 11 },
+          new { ArtworksId = 3, TagsId = 16 },
+
+          new { ArtworksId = 4, TagsId = 11 },
+
+          new { ArtworksId = 5, TagsId = 2 },
+          new { ArtworksId = 5, TagsId = 5 },
+          new { ArtworksId = 5, TagsId = 8 },
+          new { ArtworksId = 5, TagsId = 17 },
+          new { ArtworksId = 5, TagsId = 19 },
+
+          new { ArtworksId = 6, TagsId = 10 },
+
+          new { ArtworksId = 7, TagsId = 1 },
+          new { ArtworksId = 7, TagsId = 11 },
+          new { ArtworksId = 7, TagsId = 17 },
+
+          new { ArtworksId = 8, TagsId = 5 },
+
+          new { ArtworksId = 9, TagsId = 11 },
+
+          new { ArtworksId = 10, TagsId = 13 },
+          new { ArtworksId = 10, TagsId = 15 },
+
+          new { ArtworksId = 11, TagsId = 6 },
+
+          new { ArtworksId = 13, TagsId = 2 },
+          new { ArtworksId = 13, TagsId = 8 },
+          new { ArtworksId = 13, TagsId = 16 },
+
+          new { ArtworksId = 14, TagsId = 15 },
+          new { ArtworksId = 14, TagsId = 16 },
+
+          new { ArtworksId = 15, TagsId = 7 },
+          new { ArtworksId = 15, TagsId = 14 },
+
+          new { ArtworksId = 16, TagsId = 8 },
+
+          new { ArtworksId = 17, TagsId = 1 },
+          new { ArtworksId = 17, TagsId = 22 },
+
+          new { ArtworksId = 18, TagsId = 1 },
+          new { ArtworksId = 18, TagsId = 8 },
+          new { ArtworksId = 18, TagsId = 19 },
+
+          new { ArtworksId = 19, TagsId = 4 }
+        };
+    }
+  }
+}
