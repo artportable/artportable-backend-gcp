38bd40a545c5746d4c044c662e9a00a4e16698e4
Anders <andersroseen@gmail.com>
2021-05-04 10:24:20 +0200

    New endpoint for start page

---
+++ DTOs/UserImageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Artportable.API.DTOs
+{
+  public class UserImageDTO
+  {
+    public FileDTO Image { get; set; }
+    public string Username { get; set; }
+    public string ProfilePicture { get; set; }
+  }
+}
