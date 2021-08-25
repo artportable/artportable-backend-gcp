9a818c8a0cf12e8683dc42ef695f82cc44b63025
Cristian Bude <cristian@bude.se>
2021-08-25 13:40:22 +0200

    add title to feed artwork post

--- DTOs/ArtworkPostDTO.cs
+++ DTOs/ArtworkPostDTO.cs
@@ -5,6 +5,7 @@ namespace Artportable.API.DTOs
   public class ArtworkPostDTO
   {
     public Guid Id { get; set; }
+    public string Title { get; set;}
     public FileDTO PrimaryFile { get; set; }
     public FileDTO SecondaryFile { get; set; }
     public FileDTO TertiaryFile { get; set; }
