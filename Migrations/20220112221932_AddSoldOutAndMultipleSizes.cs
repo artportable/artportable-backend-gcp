a783085829e8eae8428038a136bccf4115900283
Emil Sveningson <emil@boulder.se>
2022-01-12 23:21:24 +0100

    Added DB updates for soldout and multiple sizes

---
+++ Migrations/20220112221932_AddSoldOutAndMultipleSizes.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Artportable.API.Migrations
+{
+    public partial class AddSoldOutAndMultipleSizes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "multiple_sizes",
+                table: "artworks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "sold_out",
+                table: "artworks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "multiple_sizes",
+                table: "artworks");
+
+            migrationBuilder.DropColumn(
+                name: "sold_out",
+                table: "artworks");
+        }
+    }
+}
