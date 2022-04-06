e96a02767896656aec8a71678a2515f805e7a1eb
Pontus1991 <70883949+Pontus1991@users.noreply.github.com>
2022-04-06 14:59:37 +0200

    W I P

--- Services/DiscoverService.cs
+++ Services/DiscoverService.cs
@@ -183,6 +183,7 @@ namespace Artportable.API.Services
         MonthlyUser = u.MonthlyUser,
         Random = random.Next().ToString()
       })
+      .Where(u => u.Username != myUsername)
       .Where(u => u.MonthlyUser)
       .Where(u => u.Artworks.Count() > 0)
       .Where(u => u.Subscription.ProductId != (int)ProductEnum.Bas)
@@ -329,6 +330,7 @@ namespace Artportable.API.Services
           MonthlyUser = u.MonthlyUser,
           FolloweeRef = u.FolloweeRef.Count
         })
+        .Where(u => u.Username != myUsername)
         .Where(u => u.Artworks.Count() > minArtworks)
         .Where(u => u.Subscription.ProductId >= (int)minimumProduct)
         .OrderByDescending(u => u.FolloweeRef)
@@ -435,5 +437,133 @@ namespace Artportable.API.Services
         })
         .ToList();
     }
+
+    public List<ArtworkDTO> GetTrendingArtworksSold (int page, int pageSize, List<string> tags, string myUsername, DateTime likesSince, ProductEnum minimumProduct = ProductEnum.Portfolio){
+      return _context.Artworks
+        .Where(a => tags.Count != 0 ? a.Tags.Any(t => tags.Contains(t.Title)) : true)
+        .Where(a => a.User.Subscription.ProductId >= (int)minimumProduct)
+        .OrderByDescending(a => a.Likes.Select(
+              l => new {
+                Date = l.Date
+            })
+            .Where(l => l.Date > likesSince)
+            .Count())
+        .ThenByDescending(a => a.Likes.Count())
+        .Skip(pageSize * (page - 1))
+        .Take(pageSize)
+        .Select(a =>
+        new ArtworkDTO
+        {
+          Id = a.PublicId,
+          Owner = new OwnerDTO
+          {
+            Username = a.User.Username,
+            ProfilePicture = a.User.File.Name,
+            SocialId = a.User.SocialId,
+            Name = a.User.UserProfile.Name,
+            Surname = a.User.UserProfile.Surname,
+            Location = a.User.UserProfile.Location
+          },
+          Title = a.Title,
+          Name = a.User.UserProfile.Name,
+          Surname = a.User.UserProfile.Surname,
+          Description = a.Description,
+          Published = a.Published,
+          Price = a.Price,
+          SoldOut = a.SoldOut,
+          MultipleSizes = a.MultipleSizes,
+          Width = a.Width,
+          Height = a.Height,
+          Depth = a.Depth,
+          PrimaryFile = new FileDTO
+          {
+            Name = a.PrimaryFile.Name,
+            Width = a.PrimaryFile.Width,
+            Height = a.PrimaryFile.Height
+          },
+          SecondaryFile = a.SecondaryFile != null ? new FileDTO
+          {
+            Name = a.SecondaryFile.Name,
+            Width = a.SecondaryFile.Width,
+            Height = a.SecondaryFile.Height
+          } : null,
+          TertiaryFile = a.TertiaryFile != null ? new FileDTO
+          {
+            Name = a.TertiaryFile.Name,
+            Width = a.TertiaryFile.Width,
+            Height = a.TertiaryFile.Height
+          } : null,
+          Tags = (a.Tags != null ? a.Tags.Select(t => t.Title).ToList() : new List<string>()),
+          Likes = a.Likes.Count(),
+          LikedByMe = !string.IsNullOrWhiteSpace(myUsername) ? a.Likes.Any(l => l.User.Username == myUsername) : false,
+        })
+        .ToList();
+    }
+
+    public List<ArtworkDTO> GetTrendingArtworksUnsold (int page, int pageSize, List<string> tags, string myUsername, DateTime likesSince, ProductEnum minimumProduct = ProductEnum.Portfolio){
+      return _context.Artworks
+        .Where(a => tags.Count != 0 ? a.Tags.Any(t => tags.Contains(t.Title)) : true)
+        .Where(a => a.User.Subscription.ProductId >= (int)minimumProduct)
+        .OrderByDescending(a => a.Likes.Select(
+              l => new {
+                Date = l.Date
+            })
+            .Where(l => l.Date > likesSince)
+            .Count())
+        .ThenByDescending(a => a.Likes.Count())
+        .Skip(pageSize * (page - 1))
+        .Take(pageSize)
+        .Select(a =>
+        new ArtworkDTO
+        {
+          Id = a.PublicId,
+          Owner = new OwnerDTO
+          {
+            Username = a.User.Username,
+            ProfilePicture = a.User.File.Name,
+            SocialId = a.User.SocialId,
+            Name = a.User.UserProfile.Name,
+            Surname = a.User.UserProfile.Surname,
+            Location = a.User.UserProfile.Location
+          },
+          Title = a.Title,
+          Name = a.User.UserProfile.Name,
+          Surname = a.User.UserProfile.Surname,
+          Description = a.Description,
+          Published = a.Published,
+          Price = a.Price,
+          SoldOut = a.SoldOut,
+          MultipleSizes = a.MultipleSizes,
+          Width = a.Width,
+          Height = a.Height,
+          Depth = a.Depth,
+          PrimaryFile = new FileDTO
+          {
+            Name = a.PrimaryFile.Name,
+            Width = a.PrimaryFile.Width,
+            Height = a.PrimaryFile.Height
+          },
+          SecondaryFile = a.SecondaryFile != null ? new FileDTO
+          {
+            Name = a.SecondaryFile.Name,
+            Width = a.SecondaryFile.Width,
+            Height = a.SecondaryFile.Height
+          } : null,
+          TertiaryFile = a.TertiaryFile != null ? new FileDTO
+          {
+            Name = a.TertiaryFile.Name,
+            Width = a.TertiaryFile.Width,
+            Height = a.TertiaryFile.Height
+          } : null,
+          Tags = (a.Tags != null ? a.Tags.Select(t => t.Title).ToList() : new List<string>()),
+          Likes = a.Likes.Count(),
+          LikedByMe = !string.IsNullOrWhiteSpace(myUsername) ? a.Likes.Any(l => l.User.Username == myUsername) : false,
+        })
+        .ToList();
+    }
+
+
+
+
   }
 }
--- Services/IDiscoverService.cs
+++ Services/IDiscoverService.cs
@@ -10,6 +10,8 @@ namespace Artportable.API.Services
     List<ArtworkDTO> GetArtworks(int page, int pageSize, List<string> tags, string myUsername, int seed, ProductEnum minimumProduct = ProductEnum.Portfolio);
     List<ArtworkDTO> GetTopArtworks(int page, int pageSize, List<string> tags, string myUsername, ProductEnum minimumProduct = ProductEnum.Portfolio);
     List<ArtworkDTO> GetTrendingArtworks(int page, int pageSize, List<string> tags, string myUsername, DateTime likesSince, ProductEnum minimumProduct = ProductEnum.Portfolio);
+    List<ArtworkDTO> GetTrendingArtworksSold(int page, int pageSize, List<string> tags, string myUsername, DateTime likesSince, ProductEnum minimumProduct = ProductEnum.Portfolio);
+    List<ArtworkDTO> GetTrendingArtworksUnsold(int page, int pageSize, List<string> tags, string myUsername, DateTime likesSince, ProductEnum minimumProduct = ProductEnum.Portfolio);
     List<ArtistDTO> GetArtists(int page, int pageSize, string myUsername, int seed, int minArtworks = 1, ProductEnum minimumProduct = ProductEnum.Portfolio);
     List<ArtistDTO> GetTopArtists(int page, int pageSize, string myUsername, int minArtworks = 1, ProductEnum minimumProduct = ProductEnum.Portfolio);
     List<ArtistDTO> GetMonthlyArtists(int page, int pageSize, string myUsername, int seed);
