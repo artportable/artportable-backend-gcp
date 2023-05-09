f33e8c10987bf0020677c756175550555e77242f
MassiWR <massi.wararoodi@chasacademy.se>
2023-05-09 21:14:26 +0200

    add postForCreationDTO

---
+++ DTOs/PostForCreationDTO.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+
+namespace Artportable.API.DTOs
+{
+public class PostForCreationDto
+{
+    public int UserId { get; set; }
+    public string Content { get; set; }
+}
+}
