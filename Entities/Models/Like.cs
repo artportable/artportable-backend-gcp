3a532da56300112b32605a1e11d9d2d9fc8274e0
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-05-10 10:05:54 +0200

    Revert "Feature/userposts"

--- Entities/Models/Like.cs
+++ Entities/Models/Like.cs
@@ -8,14 +8,12 @@ namespace Artportable.API.Entities.Models
     [Key]
     public int Id { get; set; }
 
+
     public int UserId { get; set; }
     public int ArtworkId { get; set; }
-    public int PostId { get; set; }
 
     public DateTime Date { get; set;}
-
     public User User { get; set; }
     public Artwork Artwork { get; set; }
-    public Post Post { get; set; }
   }
 }
