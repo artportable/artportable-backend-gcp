b8c4f6f1ca1de686e38eaeb2a78f4d61bda4e83b
MassiWR <massi.wararoodi@chasacademy.se>
2024-03-07 15:18:37 +0100

    add migration

---
+++ Migrations/20240307141812_promotedAt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Artportable.API.Migrations
+{
+    public partial class promotedAt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "promoted_at",
+                table: "artworks",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "promoted_at",
+                table: "artworks");
+        }
+    }
+}
