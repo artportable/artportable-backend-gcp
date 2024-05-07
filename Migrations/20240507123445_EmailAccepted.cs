f0199cc12a049dc007670b84acc4582595002501
MassiWR <massi.wararoodi@chasacademy.se>
2024-05-07 14:35:21 +0200

    fix table

---
+++ Migrations/20240507123445_EmailAccepted.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Artportable.API.Migrations
+{
+    public partial class EmailAccepted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "email_accepted_uppload",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_accepted_uppload",
+                table: "users");
+        }
+    }
+}
