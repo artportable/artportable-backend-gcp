ab4c0ec83549efd77c544b8e473fe936616f70f2
massiWR <massi.wararoodi@outlook.com>
2024-10-01 11:22:08 +0200

    change order

--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -57,16 +57,16 @@ namespace Artportable.API.Services
                 return productStatistics;
             }
 
-                public List<UserDTO> GetUsersByProduct(int productId)
+        public List<UserDTO> GetUsersByProduct(int productId)
             {
-                // Get the date one month ago from today
-                var oneMonthAgo = DateTime.Now.AddMonths(-2);
+                // Get the date two months ago from today
+                var twoMonthsAgo = DateTime.Now.AddMonths(-2);
 
                 var usersWithProduct = _context.Users
                     .Join(_context.Subscriptions, u => u.SubscriptionId, s => s.Id, (u, s) => new { u, s })
                     .GroupJoin(_context.Products, us => us.s.ProductId, p => p.Id, (us, products) => new { us, products })
                     .SelectMany(x => x.products.DefaultIfEmpty(), (x, p) => new { x.us, p })
-                    .Where(x => x.p != null && x.p.Id == productId && x.us.u.Created >= oneMonthAgo)
+                    .Where(x => x.p != null && x.p.Id == productId && x.us.u.Created >= twoMonthsAgo)
                     .Select(x => new UserDTO
                     {
                         Username = x.us.u.Username,
@@ -74,10 +74,12 @@ namespace Artportable.API.Services
                         Created = x.us.u.Created,
                         Name = x.us.u.UserProfile.Name,
                     })
+                    .OrderByDescending(x => x.Created) 
                     .ToList();
 
                 return usersWithProduct;
             }
+
         public List<UserWithSubscriptionDTO> GetUsersByProductSubscription(int productId)
         {
             var userIds = _context.Users
